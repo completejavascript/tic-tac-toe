77c6ec0572c3285cfb8e384c397596d98403ed92
completejavascript <completejavascript.super@gmail.com>
2019-01-25 00:00:55 +0700

    Updates

---
+++ precache-manifest.e1d70381d4283258e64ea12c06fea9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "9df4b6552650a74d5b7e",
+    "url": "/tic-tac-toe/static/js/main.9df4b655.chunk.js"
+  },
+  {
+    "revision": "b99b469af88ff7aa2a2e",
+    "url": "/tic-tac-toe/static/js/1.b99b469a.chunk.js"
+  },
+  {
+    "revision": "9df4b6552650a74d5b7e",
+    "url": "/tic-tac-toe/static/css/main.47b42c25.chunk.css"
+  },
+  {
+    "revision": "51a24e4e5c3e2cc7ac9be06a9bac4a72",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
